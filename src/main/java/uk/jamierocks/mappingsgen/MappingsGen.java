ea3125880d23a18f686a23d33b7f8bcb7a5fb192
Jamie Mansfield <malgm@lapis.blue>
2015-09-22 21:51:05 +0100

    Work on generation

---
+++ src/main/java/uk/jamierocks/mappingsgen/MappingsGen.java
@@ -0,0 +1,45 @@
+package uk.jamierocks.mappingsgen;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MappingsGen {
+
+    private static final File classRules = new File("input/client.rules");
+    private static final File classOutput = new File("output/classes.srg");
+
+    private static BiMap<String, String> classMappings;
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream(classRules);
+        //if (!classOutput.exists()) classOutput.createNewFile();
+        FileOutputStream fos = new FileOutputStream(classOutput);
+        Scanner scanner = new Scanner(fis);
+
+        ImmutableBiMap.Builder b = ImmutableBiMap.<String, String>builder();
+
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            line = line.replace("rule ", "");
+
+            String[] mapping = line.split(" ");
+            b.put(mapping[0].replace(" ", ""), mapping[1].replace(" ", ""));
+
+            String classLine = String.format("CL: %s %s\n", mapping[0], mapping[1]);
+            byte[] data = classLine.getBytes();
+
+            fos.write(data, 0, data.length);
+        }
+
+        classMappings = b.build();
+
+        fis.close();
+        fos.close();
+    }
+}

53ae872b05a643a8ed4aaf6d5c328721f267fa29
Jamie Mansfield <malgm@lapis.blue>
2015-09-26 17:02:10 +0100

    Don't need to store field values

--- src/main/java/uk/jamierocks/mappingsgen/MappingsGen.java
+++ src/main/java/uk/jamierocks/mappingsgen/MappingsGen.java
@@ -1,13 +1,11 @@
 package uk.jamierocks.mappingsgen;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.Maps;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -17,14 +15,12 @@ public class MappingsGen {
     private static final File classRgs = new File("input/client.rgs");
     private static final File classOutput = new File("output/classes.srg");
 
-    private static Map<String, String> classMappings = new HashMap<>();
-    private static Map<String, String> fieldMappings = new HashMap<>();
+    private static Map<String, String> classMappings = Maps.newHashMap();
 
     public static void main(String[] args) throws IOException {
         FileInputStream rules = new FileInputStream(classRules);
         FileInputStream rgs = new FileInputStream(classRgs);
-        //if (!classOutput.exists()) classOutput.createNewFile();
-        FileOutputStream fos = new FileOutputStream(classOutput);
+        FileOutputStream srgFile = new FileOutputStream(classOutput);
         Scanner rulesScanner = new Scanner(rules);
         Scanner rgsScanner = new Scanner(rgs);
 
@@ -39,7 +35,7 @@ public class MappingsGen {
 
             if (!classLine.contains("@")) {
                 classMappings.put(mapping[1].replace(".", "/"), mapping[0].replace(".", "/"));
-                fos.write(data, 0, data.length);
+                srgFile.write(data, 0, data.length);
             }
         }
 
@@ -63,6 +59,7 @@ public class MappingsGen {
                 String obfuscated = gg + "/" + thing;
                 System.out.println(className);
                 System.out.println(gg);
+                System.out.println(obfuscated);
                 System.out.println(" ");
                 String deobfuscated = className + "/" + mapping[1];
 
@@ -70,13 +67,12 @@ public class MappingsGen {
                 byte[] data = fieldLine.getBytes();
 
                 if (!fieldLine.contains("$") && !thing.equalsIgnoreCase(mapping[1])) {
-                    fieldMappings.put(deobfuscated, obfuscated);
-                    fos.write(data, 0, data.length);
+                    srgFile.write(data, 0, data.length);
                 }
             }
         }
 
         rules.close();
-        fos.close();
+        srgFile.close();
     }
 }
